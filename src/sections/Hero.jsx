df0d986d89c106b4ab11d5255c94883af3bfe6ec
gaudakash <akashgauda16@gmail.com>
2026-06-02 08:18:41 +0530

    Updated cv

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -95,7 +95,7 @@ export const Hero = () => {
                 </Button>
               </a>
 
-              <a href="./projects/Akash_Gauda_Developer.pdf" download>
+              <a href="./projects/AKASH_FULL_STACK_DEVELOPER.pdf" download>
                 <AnimatedBorderButton>
                   <Download className="w-5 h-5" />
                   Download CV
